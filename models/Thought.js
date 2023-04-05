58cede3fcdaa02d70460ca5ef3c5691ef65e9218
Shannya <niveyro101@gmail.com>
2023-04-04 21:10:22 -0400

    thought model made with virtual

--- models/Thought.js
+++ models/Thought.js
@@ -15,6 +15,26 @@ const thoughtSchema = new Schema (
         createdAt : {
             type: Date,
             default: Date.now,
-        }
+            //getter method to format the timestamp on query?
+        },
+        username: {
+            type: String,
+            ref: 'user',
+            required: true,
+        },
+        reactions : [
+                reactionSchema
+        ]
+    },
+    {
+        toJSON:{
+            virtuals: true, 
+            getters: true,
+        },
+        id: false,
     }
 )
+
+thoughtSchema.virtual('reactionCount').get(function(){ 
+    return this.reactions.length;
+})
